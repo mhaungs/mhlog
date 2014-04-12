fe50556bce260cc9960737939988b93bc93e7c01
Michael Haungs <mhaungs@calpoly.edu>
2014-04-12 15:49:02 -0700

    Added testing fixes.  Tried Karma out.

---
+++ src/test/mhLogTest.js
@@ -0,0 +1,16 @@
+test("hello test", function() {
+    ok(1 == "1", "Passed!");
+});
+
+/*test("existence test", function() {
+ var mhlog = module.exports();
+ ok(mhlog, "mhlog Exists");
+ ok(mhlog.log, "mhlog.log Exists");
+ equal(mhlog.LEVEL.DEBUG, 1, "mhlog.LEVEL.DEBUG is 1");
+ ok(mhlog.LEVEL.ALL < mhlog.LEVEL.PRODUCTION, "mhlog.LEVEL is ordered");
+ });
+
+ test("log test", function() {
+ var mhlog = module.exports();
+ equal(mhlog.log(mhlog.LEVEL.PRODUCTION, "hi"), "hi", "log level PRODUCTION works");
+ });*/
